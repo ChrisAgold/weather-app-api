8e7d0921d07aae88362384114d9ee74bc646f902
Chris Agold <christophersagold@gmail.com>
2021-01-06 23:17:16 -0600

    added forecast js

--- forecast.js
+++ forecast.js
@@ -1,68 +1,101 @@
-// MapBox //
-mapboxgl.accessToken = mapboxToken;
-var coordinates = document.getElementById('coordinates');
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/navigation-preview-night-v4',
-  center: [-98.4916, 29.4252],
-  zoom: 9
-});
-// Marker
-var marker = new mapboxgl.Marker({
-  draggable: true
-})
-  .setLngLat([-98.4916, 29.4252])
-  .addTo(map);
-// Drag Marker
-function onDragEnd() {
-  var lngLat = marker.getLngLat();
-  coordinates.style.display = 'block';
-  coordinates.innerHTML =
-    'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
-}
+// // Forcast JS //
+// $(document).ready(function () {
+//
+//   $("#submitForecast").click(function () {
+//     return getForecast();
+//   });
+// });
+//
+// function getForecast() {
+//   var city = $("#city").val();
+//   var days = $("#days").val();
+//
+//   if (city) !='' && days != ''){
+//
+//     $.ajax({
+//       url: 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + "&units=imperial" + "&cnt=5" + "&APPID=" + weatherToken,
+//       type: "GET",
+//       dataType: "jsonp",
+//       success: function (data) {
+//         var table = '';
+//
+//         for (var i = 0; i < data.list.length; i++) {
+//           table += "<tr>";
+//
+//           table += "<td>" + data.list[i].weather[0].icon + "</td>"
+//           table += "<td>" + data.list[i].weather[0].main + "</td>"
+//           table += "<td>" + data.list[i].weather[0].description + "</td>"
+//
+//           table += "</tr>"
+//         }
+//         $("#forecastWeather").html(table);
+//         var city = $("#city").val('');
+//       }
+//     });
+//   }
+// else
+//   {
+//     $("#error-message").html("<div class='alert alert-danger' id='errorCity'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>City field cannot be empty</div>");
+//   }
+// }
 
-marker.on('dragend', onDragEnd);
-
-
-// Search
-let searchString;
-
-// Add an event listener for the search-button 'button'
-document.getElementById("city").addEventListener("click", function(){
-  searchString = "submitForecast";
-  geocode(searchString, mapboxToken).then(function(result){
-    console.log(result);
-    // map.setCenter(result); // i.e. map.setCenter([-98.48, 29.426])
-    map.flyTo({
-      center: result,
-      zoom: 14,
-      speed: 4,
-      curve: 1,
-    })
-    marker.setLngLat(result);
-
-    // Want to add a popup that displays the name of the location at the LONG / LAT coordinates we just found
-    reverseGeocode(result, mapboxToken).then(function(placeName) {
-      // set the text of the popup to "New York City" (for example)
-      console.log("after reverse geocode, the place name is: " + placeName);
-      popup.setText(placeName);
-      // the popup is already added to the marker, so NO need to add it again with marker.setPopup!
-
-    })
-  })
-});
-
-// Forcast JS //
 $(document).ready(function(){
 
   $("#submitForecast").click(function(){
-return getForecast()
+    return getForecast();
   });
+
 });
 
-function getForecast() {
-  
+function getForecast(){
+  var city = $("#city").val();
+  var days = $("#days").val();
+
+  if(city != '' && days != ''){
+
+    $.ajax({
+      url: 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + "&units=metric" + "&cnt=5" + "&APPID=" + weatherToken,
+      type: "GET",
+      dataType: "jsonp",
+      success: function(data){
+
+        var table = '';
+
+        var header = '<h2 style="font-weight:bold; font-size:30px; margin-top:20px;">Weather forecast for ' + data.city.name + ', ' + data.city.country + '</h2>'
+
+        for(var i = 0; i < data.list.length; i++){
+          table += "<tr>";
+
+          table += "<td><img src='http://openweathermap.org/img/w/"+data.list[i].weather[0].icon+".png'></td>";
+          table += "<td>" + data.list[i].weather[0].main + "</td>";
+          table += "<td>" + data.list[i].weather[0].description + "</td>";
+          table += "<td>" + data.list[i].temp.morn + "&deg;C</td>";
+          table += "<td>" + data.list[i].temp.night + "&deg;C</td>";
+          table += "<td>" + data.list[i].temp.min + "&deg;C</td>";
+          table += "<td>" + data.list[i].temp.max + "&deg;C</td>";
+          table += "<td>" + data.list[i].pressure + "hpa</td>";
+          table += "<td>" + data.list[i].humidity + "%</td>";
+          table += "<td>" + data.list[i].speed + "m/s</td>";
+          table += "<td>" + data.list[i].deg + "&deg;</td>";
+
+
+          table += "</tr>";
+        }
+
+        $("#forecastWeather").html(table);
+        $("#header").html(header);
+
+        $("#city").val('');
+        $("#days").val('')
+
+      }
+
+
+    });
+
+  }else{
+    $("#error").html("<div class='alert alert-danger' id='errorCity'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>City field cannot be empty</div>");
+  }
+
 }
-
-
 
---
+++ mapbox.js
@@ -0,0 +1,53 @@
+// MapBox //
+mapboxgl.accessToken = mapboxToken;
+var coordinates = document.getElementById('coordinates');
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/navigation-preview-night-v4',
+  center: [-98.4916, 29.4252],
+  zoom: 9
+});
+// Marker
+var marker = new mapboxgl.Marker({
+  draggable: true
+})
+  .setLngLat([-98.4916, 29.4252])
+  .addTo(map);
+// Drag Marker
+function onDragEnd() {
+  var lngLat = marker.getLngLat();
+  coordinates.style.display = 'block';
+  coordinates.innerHTML =
+    'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
+}
+
+marker.on('dragend', onDragEnd);
+
+
+// Search
+let searchString;
+
+// Add an event listener for the search-button 'button'
+document.getElementById("city").addEventListener("click", function(){
+  searchString = "submitForecast";
+  geocode(searchString, mapboxToken).then(function(result){
+    console.log(result);
+    // map.setCenter(result); // i.e. map.setCenter([-98.48, 29.426])
+    map.flyTo({
+      center: result,
+      zoom: 14,
+      speed: 4,
+      curve: 1,
+    })
+    marker.setLngLat(result);
+
+    // Want to add a popup that displays the name of the location at the LONG / LAT coordinates we just found
+    reverseGeocode(result, mapboxToken).then(function(placeName) {
+      // set the text of the popup to "New York City" (for example)
+      console.log("after reverse geocode, the place name is: " + placeName);
+      popup.setText(placeName);
+      // the popup is already added to the marker, so NO need to add it again with marker.setPopup!
+
+    })
+  })
+});
